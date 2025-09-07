8f0aa7fa1c20fb76ea8050b949194b2b56ae6cdd
Sajeewa Dissa <46420337+Sajeewa-Dissa@users.noreply.github.com>
2025-09-07 16:39:45 +0100

    Updated Unit Test to call all API methods

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using SimpleApi1.Controllers;
 using Xunit;
 
@@ -17,12 +18,31 @@ namespace SimpleApi1.Test
         [Fact]
         public void Test1()
         {
+            // Extract the value
+            var actualList = controller.Get().Value?.ToList() ?? new List<string>(); // Q. does this ever return null?
 
+            List<string> expectedValues = new List<string> { "value1", "value2", "value3" };
+
+            // Assert
+            Assert.Equal(expectedValues.Count, actualList.Count);
+            Assert.True(expectedValues.All(expected => actualList.Contains(expected)),
+                "The IEnumerable does not contain all expected values.");
         }
 
         [Fact]
         public void Test2()
         {
+            string input = "HelloWorld";
+            string expected = $"Called values Post method passing in string {input}";
+
+            // Act
+            ActionResult<string> result = controller.PostValue(input);
+
+            // Assert
+            var okResult = Assert.IsType<ActionResult<string>>(result);
+            var value = Assert.IsType<string>(okResult.Value);
+            Assert.Equal(expected, value);
+
             Assert.Equal("aa", "AA", true);
             Assert.Equal(12, 12);
         }
