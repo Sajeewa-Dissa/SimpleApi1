5c97a8072da891af4f8b61404ab5c4c6049451aa
Sajeewa Dissa <46420337+Sajeewa-Dissa@users.noreply.github.com>
2025-09-11 18:56:09 +0100

    Use standard API routing

--- src/Controllers/ValuesController.cs
+++ src/Controllers/ValuesController.cs
@@ -6,14 +6,16 @@ namespace SimpleApi1.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        //GET api/values/
+        //GET api/values/ e.g. https://localhost:7151/api/values/
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            Console.WriteLine("hello to console"); //write to command window
+            System.Diagnostics.Debug.WriteLine("hello to debug"); //write to VS Outpput window
             return new string[] { "value1","value2","value3" };
         }
 
-        //GET api/values/getValue/5
+        //GET api/values/5 e.g. https://localhost:7151/api/values/5
         [HttpGet("{id}")]
         public ContentResult Get(int id)
         {
